8fbab1dd7ba194aefe9069e401786dbaa6ed1f50
zhuziwuming <79568723+zhuziwuming@users.noreply.github.com>
2023-08-15 14:10:27 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -1037,6 +1037,7 @@ public class PlayFragment extends BaseLazyFragment {
                 }
             }
 		webUserAgent ="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36";
+		webHeaderMap.put("User-Agent",webUserAgent);
             loadWebView(pb.getUrl() + webUrl);
 
         } else if (pb.getType() == 1) { // json 解析
