58bba1d0be077d07e0a0f2ad36f4a72973681dfb
zhuziwuming <79568723+zhuziwuming@users.noreply.github.com>
2023-11-08 22:14:37 +0800

    Update AbsJson.java

--- app/src/main/java/com/github/tvbox/osc/bean/AbsJson.java
+++ app/src/main/java/com/github/tvbox/osc/bean/AbsJson.java
@@ -109,7 +109,7 @@ public class AbsJson implements Serializable {
             Movie.Video video = new Movie.Video();
             video.tag = vod_tag;
             video.last = vod_time;
-            if (vod_down_url.contains("aliyun.com")) {  
+            if (vod_down_url.contains("aliyundrive.com")) {  
                 vod_id = "push://" + vod_down_url ;  
             }
             video.id = vod_id;
