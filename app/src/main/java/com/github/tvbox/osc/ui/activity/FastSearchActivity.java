d2560919c2131cde525a89f4c7296b297513de56
zhuziwuming <79568723+zhuziwuming@users.noreply.github.com>
2023-12-08 09:25:59 +0800

    Update FastSearchActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/FastSearchActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/FastSearchActivity.java
@@ -246,6 +246,7 @@ public class FastSearchActivity extends BaseActivity {
         searchWordAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+				cancel();
                 String str = searchWordAdapter.getData().get(position).trim();
                 search(str);
             }
