a82cf826ac68154184bb493d77e7c3e8834e0f4c
zhuziwuming <79568723+zhuziwuming@users.noreply.github.com>
2024-01-06 02:49:16 +0800

    更新 PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -980,7 +980,7 @@ public class PlayFragment extends BaseLazyFragment {
             }
 
             @Override
-            public void list(String playList) {
+            public void list(Map<Integer, String> urlMap) {
             }
 
             @Override
