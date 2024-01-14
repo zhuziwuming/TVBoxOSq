dc1a9d10dba149e735e5a23cf410817b2dc5e3bc
zhuziwuming <79568723+zhuziwuming@users.noreply.github.com>
2024-01-15 01:59:17 +0800

    tt

---
+++ app/src/main/java/com/github/catvod/crawler/Spider.java
@@ -0,0 +1,68 @@
+package com.github.catvod.crawler;
+
+import android.content.Context;
+import com.github.tvbox.osc.util.OkGoHelper;
+import com.github.tvbox.osc.util.js.Connect;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import okhttp3.Dns;
+
+public abstract class Spider {
+
+    public void init(Context context) throws Exception {}
+
+    public void init(Context context, String extend) throws Exception {
+        init(context);
+    }
+
+    public String homeContent(boolean filter) throws Exception {
+        return "";
+    }
+
+    public String homeVideoContent() throws Exception {
+        return "";
+    }
+
+    public String categoryContent(String tid, String pg, boolean filter, HashMap < String, String > extend) throws Exception {
+        return "";
+    }
+
+    public String detailContent(List < String > ids) throws Exception {
+        return "";
+    }
+
+    public String searchContent(String key, boolean quick) throws Exception {
+        return "";
+    }
+
+    public String searchContent(String key, boolean quick, String pg) throws Exception {
+        return "";
+    }
+
+    public String playerContent(String flag, String id, List < String > vipFlags) throws Exception {
+        return "";
+    }
+
+    public boolean manualVideoCheck() throws Exception {
+        return false;
+    }
+
+    public boolean isVideoFormat(String url) throws Exception {
+        return false;
+    }
+
+    public Object[] proxyLocal(Map < String, String > params) throws Exception {
+        return null;
+    }
+
+    public void cancelByTag() {
+
+    }
+
+    public void destroy() {}
+
+    public static Dns safeDns() {
+        return OkGoHelper.dnsOverHttps;
+    }
+}
---
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -0,0 +1,118 @@
+package com.github.tvbox.osc.base;
+
+import android.app.Activity;
+import androidx.multidex.MultiDexApplication;
+import com.github.catvod.crawler.JsLoader;
+import com.github.tvbox.osc.bean.VodInfo;
+import com.github.tvbox.osc.callback.EmptyCallback;
+import com.github.tvbox.osc.callback.LoadingCallback;
+import com.github.tvbox.osc.data.AppDataManager;
+import com.github.tvbox.osc.server.ControlManager;
+import com.github.tvbox.osc.util.AppManager;
+import com.github.tvbox.osc.util.EpgUtil;
+import com.github.tvbox.osc.util.FileUtils;
+import com.github.tvbox.osc.util.HawkConfig;
+import com.github.tvbox.osc.util.LOG;
+import com.github.tvbox.osc.util.OkGoHelper;
+import com.github.tvbox.osc.util.PlayerHelper;
+
+import com.kingja.loadsir.core.LoadSir;
+import com.orhanobut.hawk.Hawk;
+import com.p2p.P2PClass;
+import com.whl.quickjs.android.QuickJSLoader;
+import me.jessyan.autosize.AutoSizeConfig;
+import me.jessyan.autosize.unit.Subunits;
+
+/**
+ * @author pj567
+ * @date :2020/12/17
+ * @description:
+ */
+public class App extends MultiDexApplication {
+    private static App instance;
+
+    private static P2PClass p;
+    public static String burl;
+    private static String dashData;
+    private VodInfo vodInfo;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+        initParams();
+        // OKGo
+        OkGoHelper.init(); //台标获取
+        EpgUtil.init();
+        // 初始化Web服务器
+        ControlManager.init(this);
+        //初始化数据库
+        AppDataManager.init();
+        LoadSir.beginBuilder()
+                .addCallback(new EmptyCallback())
+                .addCallback(new LoadingCallback())
+                .commit();
+        AutoSizeConfig.getInstance().setCustomFragment(true).getUnitsManager()
+                .setSupportDP(false)
+                .setSupportSP(false)
+                .setSupportSubunits(Subunits.MM);
+        PlayerHelper.init();
+        QuickJSLoader.init();
+        FileUtils.cleanPlayerCache();
+    }
+
+    private void initParams() {
+        // Hawk
+        Hawk.init(this).build();
+        Hawk.put(HawkConfig.DEBUG_OPEN, false);
+        putDefault(HawkConfig.HOME_REC, 0);       // Home Rec 0=豆瓣, 1=推荐, 2=历史
+        putDefault(HawkConfig.PLAY_TYPE, 1);      // Player   0=系统, 1=IJK, 2=Exo
+        putDefault(HawkConfig.IJK_CODEC, "硬解码");// IJK Render 软解码, 硬解码
+    }
+
+    public static App getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        JsLoader.load();
+    }
+	
+	private void putDefault(String key, Object value) {
+        if (!Hawk.contains(key)) {
+            Hawk.put(key, value);
+        }
+    }
+
+    
+    public void setVodInfo(VodInfo vodinfo){
+        this.vodInfo = vodinfo;
+    }
+    public VodInfo getVodInfo(){
+        return this.vodInfo;
+    }
+
+    public static P2PClass getp2p() {
+        try {
+            if (p == null) {
+                p = new P2PClass(instance.getExternalCacheDir().getAbsolutePath());
+            }
+            return p;
+        } catch (Exception e) {
+            LOG.e(e.toString());
+            return null;
+        }
+    }
+
+    public Activity getCurrentActivity() {
+        return AppManager.getInstance().currentActivity();
+    }
+
+    public void setDashData(String data) {
+        dashData = data;
+    }
+    public String getDashData() {
+        return dashData;
+    }
+}
