66f49e553a23d4ad770b457ed47edce9f015baac
zhuziwuming <79568723+zhuziwuming@users.noreply.github.com>
2024-01-15 00:46:54 +0800

    更新 OkGoHelper.java

--- app/src/main/java/com/github/tvbox/osc/util/OkGoHelper.java
+++ app/src/main/java/com/github/tvbox/osc/util/OkGoHelper.java
@@ -26,7 +26,9 @@ import java.util.logging.Level;
 
 import javax.net.ssl.SSLSocketFactory;
 
-
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.Cache;
 import okhttp3.ConnectionSpec;
 import okhttp3.HttpUrl;
