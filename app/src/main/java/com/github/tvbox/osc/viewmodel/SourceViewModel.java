041aa2e1b67a1cbdc7b17b18c8e24f237981c83e
zhuziwuming <79568723+zhuziwuming@users.noreply.github.com>
2024-01-15 09:03:23 +0800

    更新 SourceViewModel.java

--- app/src/main/java/com/github/tvbox/osc/viewmodel/SourceViewModel.java
+++ app/src/main/java/com/github/tvbox/osc/viewmodel/SourceViewModel.java
@@ -505,9 +505,6 @@ public class SourceViewModel extends ViewModel {
                 }
             } else {
                 pushUrl = URLDecoder.decode(pushUrl);
-				if(pushUrl.contains("apipan")){
-					pushUrl = pushUrl.replace("apipan", "aliyundrive");
-				}
             }
             sourceKey = "push_agent";
             urlid = pushUrl;
